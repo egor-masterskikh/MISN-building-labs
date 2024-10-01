4911507d8ad544331faa0a437aa42d32cd6cb291
Egor Masterskikh <masterskikh.egor@gmail.com>
2024-10-01 16:44:16 +0200

    упростить конвертацию pandoc для нескольких файлов

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
-from jinja2 import Environment, select_autoescape, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 import pypandoc
-
-loader = FileSystemLoader(['lab1', '.'])
+from pathlib import Path
 
 env = Environment(
-    loader=loader,
+    loader=FileSystemLoader('.'),
     autoescape=select_autoescape()
 )
 
-lab1_name = "Исследование временно-частотных характеристик сигналов разной формы и амплитудно-модулированных сигналов"
-
-lab1 = env.get_template("lab1.md").render(
-    lab_num=1,
-    lab_name=lab1_name
+paths = (
+    'lab1/lab1.md',
+    'lab2/lab2.md'
 )
 
-output = pypandoc.convert_text(
-    source=lab1, format='md',
-    outputfile='lab1/lab1.html', to='html',
-    extra_args=(
-        '--mathjax',
-        '--standalone',
-        '-V', 'lang=ru',
-        '--metadata', f'title={lab1_name}'
+for path in paths:
+    template = env.get_template(path)
+    pypandoc.convert_text(
+        source=template.render(), format='md',
+        outputfile=Path(path).with_suffix('.html'), to='html',
+        extra_args=('--mathjax', '--standalone')
     )
-)
