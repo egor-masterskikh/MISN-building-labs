b22f07bd4da365c38a908a6e38d0d2b4b295314e
Egor Masterskikh <masterskikh.egor@gmail.com>
2024-12-02 15:28:24 +0200

    lab7: добавить график передаточной характеристики декодера и напряжения второго нуля (с другим знаком)

--- lab7/plot_coder_characteristic.m
+++ lab7/plot_coder_characteristic.m
@@ -8,7 +8,7 @@ function characteristic = plot_characteristic(table_filepath, column_name)
     x_mid = floor(points_num / 2);
 
     x_left = 1:x_mid;
-    x_right = x_mid + 1:points_num;
+    x_right = (x_mid + 1):points_num;
 
     y_left = transpose(table{x_left, column_name});
     y_right = transpose(table{x_right, column_name});
@@ -40,11 +40,14 @@ end
 coder_input_voltage = plot_characteristic( ...
     'characteristics_tables/coder.csv', 'input_voltage' ...
 );
-
 coder_in_segment_step = plot_characteristic( ...
     'characteristics_tables/coder.csv', 'in_segment_step' ...
 );
 
+figure( ...
+    'Name', 'Coder Characteristic', ...
+    'NumberTitle', 'off' ...
+);
 
 subplot(2, 1, 1);
 plot(coder_input_voltage.x, coder_input_voltage.y, 'ob');
@@ -52,7 +55,7 @@ hold on;
 plot(coder_input_voltage.xx_left, coder_input_voltage.yy_left, 'Color', '#eb6f40');
 hold on;
 plot(coder_input_voltage.xx_right, coder_input_voltage.yy_right, 'Color', '#eb6f40');
-ylabel('input voltage, V');
+ylabel('Input Voltage, V');
 
 subplot(2, 1, 2);
 plot(coder_in_segment_step.x, coder_in_segment_step.y, 'ob');
@@ -60,5 +63,35 @@ hold on;
 plot(coder_in_segment_step.xx_left, coder_in_segment_step.yy_left, 'Color', '#eb6f40');
 hold on;
 plot(coder_in_segment_step.xx_right, coder_in_segment_step.yy_right, 'Color', '#eb6f40');
-xlabel('input code index');
-ylabel('step in segment, mV');
+xlabel('Input Code Index');
+ylabel('Step in Segment, mV');
+
+
+decoder_input_voltage = plot_characteristic( ...
+    'characteristics_tables/decoder.csv', 'input_voltage' ...
+);
+decoder_in_segment_step = plot_characteristic( ...
+    'characteristics_tables/decoder.csv', 'in_segment_step' ...
+);
+
+figure( ...
+    'Name', 'Decoder Characteristic', ...
+    'NumberTitle', 'off' ...
+);
+
+subplot(2, 1, 1);
+plot(decoder_input_voltage.x, decoder_input_voltage.y, 'ob');
+hold on;
+plot(decoder_input_voltage.xx_left, decoder_input_voltage.yy_left, 'Color', '#34a853');
+hold on;
+plot(decoder_input_voltage.xx_right, decoder_input_voltage.yy_right, 'Color', '#34a853');
+ylabel('Input Voltage, V');
+
+subplot(2, 1, 2);
+plot(decoder_in_segment_step.x, decoder_in_segment_step.y, 'ob');
+hold on;
+plot(decoder_in_segment_step.xx_left, decoder_in_segment_step.yy_left, 'Color', '#34a853');
+hold on;
+plot(decoder_in_segment_step.xx_right, decoder_in_segment_step.yy_right, 'Color', '#34a853');
+xlabel('Input Code Index');
+ylabel('Step in Segment, mV');
